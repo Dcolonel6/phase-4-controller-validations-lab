978b80775e1fb8111717703343103e58f180fe88
Dennis Njogu <dennis.njogu@student.moringaschool.com>
2022-11-27 16:20:43 +0300

    done, all tests are passing

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :category, inclusion: {in:["Fiction", "Non-Fiction"]}
+    validates :content, length: { minimum: 100}
+    
+
+    # def validate_category
+    #     unless category == "Fiction" or category == "Non-Fiction"
+    #         errors.add(:category,"Category should be either Fiction or Non-Fiction")
+    #     end
+    # end
+
+    # def validate_content
+    #     if content.length <= 100
+    #         errors.add(:content,"Content is less than 100 characters")
+    #     end
+    # end
+
 end
